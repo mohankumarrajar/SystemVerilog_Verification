763f27368b5bfd4fe0c894e6e4d66f78cc4e1fcb
MOHANKUMAR.R <mohankumareee6105@gmail.com>
2025-10-15 13:00:40 +0530

    create Design.sv

--- D_FF_Verification/Design.sv
+++ D_FF_Verification/Design.sv
@@ -1,9 +1,14 @@
-module d_ff(input logic clk, rst, d, output logic q);
+module d_ff(
+  input logic clk, rst, d,
+  output logic q
+);
+  
   always @(posedge clk) begin
     if (rst)
       q <= 0;
     else
       q <= d;
   end
+  
 endmodule
 
